34781f9d27230b8e7c0a6a1bd7fd3d1073df4066
Accun02 <fhermopelufo@uade.edu.ar>
2024-10-27 23:30:32 -0300

    ondie

--- Assets/Scripts/Characters/Player/PlayerHealt.cs
+++ Assets/Scripts/Characters/Player/PlayerHealt.cs
@@ -8,10 +8,15 @@ using UnityEngine.Events;
 public class PlayerHealt : MonoBehaviour
 {
     private float life = 3f;
+    [SerializeField] private UnityEvent OnDie;
 
     public void TakeDamage(int damage)
     {
         life -= damage;
         Debug.Log(life);
+        if (life == 0)
+        {
+            OnDie.Invoke();
+        }    
     }
 }
