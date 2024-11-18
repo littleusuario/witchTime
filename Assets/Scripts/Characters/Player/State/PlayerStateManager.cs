89d8f1bb7a479a46dff3106674294955149eaabf
SpongeOfGod <157647080+SpongeOfGod@users.noreply.github.com>
2024-11-18 15:36:56 -0300

    CORTAR PIERNAS

--- Assets/Scripts/Characters/Player/State/PlayerStateManager.cs
+++ Assets/Scripts/Characters/Player/State/PlayerStateManager.cs
@@ -2,13 +2,11 @@ public class PlayerStateManager
 {
     public IState currentState;
     public State_Walking state_Walking;
-    public State_Jump state_Jumping;
     public State_Attack state_Attacking;
     public State_PlayerDeath state_PlayerDeath;
     public PlayerStateManager(RandomMovement randomMovement) 
     {
         state_Walking = new State_Walking(randomMovement, this);
-        state_Jumping = new State_Jump(randomMovement, this);
         state_Attacking = new State_Attack(randomMovement, this);
         state_PlayerDeath = new State_PlayerDeath();
 
